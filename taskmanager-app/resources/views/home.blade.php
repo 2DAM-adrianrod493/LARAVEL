a26fba975585797c27513a02490615f10af49c07
adria <arodrod0708@g.educaand.es>
2025-02-24 13:00:53 +0100

    Cambios Finales

--- taskmanager-app/resources/views/home.blade.php
+++ taskmanager-app/resources/views/home.blade.php
@@ -5,14 +5,14 @@
 @section('contenido')
 
     <div class="d-flex flex-column justify-content-center align-items-center" 
-    style="background-color: #000000; padding: 15px 0; border-radius: 15px; height: 120px">
+        style="background-color: #000000; padding: 15px 0; border-radius: 15px; height: 130px">
 
-    <!-- Botón Nuevo Proyecto -->
-    <a href="{{ route('proyectos.index') }}"
-    class="btn mb-2"
-    style="background-color: #FFFFFF; color: #000000; border-radius: 15px;">
-        Ver Proyectos
-    </a>
+        <!-- Botón Nuevo Proyecto -->
+        <a href="{{ route('proyectos.index') }}"
+        class="btn mb-2"
+        style="background-color: #FFFFFF; color: #000000; border-radius: 15px; font-size: 1.5rem;">
+            Ver Proyectos
+        </a>
 </div>
 
 @endsection
